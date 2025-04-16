7e059d31f7566570bb46d38c4c514ff248a6fea9
nishchal-buku <nishchal.poornadithya@bukuwarung.com>
2025-04-16 22:56:01 +0530

    projectsList: project set up

---
+++ projects-list/src/Home.js
@@ -0,0 +1,11 @@
+
+import ProjectList from './components/projects-list/ProjectsList';
+function Home() {
+  return (
+    <div className="App">
+      <ProjectList />
+    </div>
+  );
+}
+
+export default Home;
---
+++ projects-list/src/components/projects-list/ProjectsList.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+const ProjectsList = () => {
+
+    useEffect(() => {
+
+    }, []);
+
+
+    return (
+        <div className='projects-list-container'>
+
+        </div>
+    )
+
+}
+
+export default ProjectsList;
